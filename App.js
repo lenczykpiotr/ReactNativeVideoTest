9934f5d38b7756b53d6eb970da6f8ee06d5c3b27
Piotr Lenczyk <piotr.lenczyk@miquido.com>
2021-11-10 08:33:58 +0100

    Okta/RNVideo issue

--- App.js
+++ App.js
@@ -5,69 +5,145 @@
  * @format
  * @flow strict-local
  */
-
-import React, {useState} from 'react';
+import Video from 'react-native-video';
+import React, {useEffect, useState} from 'react';
 import type {Node} from 'react';
 import {
-  Dimensions,
+  clearTokens,
+  createConfig,
+  getAccessToken,
+  getUser,
+  revokeAccessToken,
+  revokeIdToken,
+  signIn,
+} from '@okta/okta-react-native';
+
+import {
+  Button,
   SafeAreaView,
   ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  TouchableOpacity,
-  useColorScheme,
+  TextInput,
   View,
 } from 'react-native';
-import Video from 'react-native-video';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+const config = {
+  oidc: {
+    clientId: '0oa2sjpicZ5GC7Ol31d6',
+    discoveryUri: 'https://id-preview.dolby.com/oauth2/aus11jir98E5qagEi1d7',
+    endSessionRedirectUri: 'com.valtech.dolby.exphub:/',
+    redirectUri: 'com.valtech.dolby.exphub:/callback',
+    requireHardwareBackedKeyStore: false,
+    scopes: [
+      'openid',
+      'groups',
+      'profile',
+      'email',
+      'offline_access',
+      'xhub:cache',
+    ],
+    tokenUrl:
+      'https://id-preview.dolby.com/oauth2/aus11jir98E5qagEi1d7/v1/token?device_id=1d12d0ab7a6067e3',
+  },
+};
 
-const hevcUri =
-  'https://experience-media.dolby.com/ott/6d3aa3bd-e647-4292-a9ac-acbc003dea7d/VDAFsurround/unencrypted/dash/manifest_SDR_HEVC_Only.mpd';
-const dolbyUri =
-  'https://experience-media.dolby.com/ott/6d3aa3bd-e647-4292-a9ac-acbc003dea7d/VDAFsurround/unencrypted/dash/manifest_P5_DVH1_Only.mpd';
-const dolbyHevcUri =
-  'https://experience-media.dolby.com/ott/6d3aa3bd-e647-4292-a9ac-acbc003dea7d/VDAFsurround/unencrypted/dash/manifest.mpd';
-
-const uri = hevcUri;
-// const uri = dolbyUri;
-// const uri = dolbyHevcUri;
-
-const App: () => Node = () => {
-  const [error, setError] = useState('');
-  const isDarkMode = useColorScheme() === 'dark';
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+const AuthApp: () => Node = () => {
+  const [isInit, setIsInit] = useState(false);
+  const [isLoading, setIsLoading] = useState(isLoading);
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const initAuth = async () => {
+    try {
+      setIsInit(true);
+      await createConfig(config.oidc);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsInit(false);
+    }
   };
 
+  const login = async () => {
+    console.log('login', {username, password});
+    setIsLoading(true);
+    try {
+      await signIn({username, password});
+      const profile = await getUser();
+      setUser(profile);
+      setPassword('');
+      setUsername('');
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const logout = async () => {
+    setIsLoading(true);
+    try {
+      await revokeAccessToken(); // optional
+      await revokeIdToken(); // optional
+      await clearTokens();
+      setUser(null);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    initAuth();
+  }, []);
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Text>{uri}</Text>
+    <SafeAreaView>
+      <ScrollView>
         <Video
-          onLoad={() => setError('')}
-          onError={err => {
-            console.log(err);
-            setError(JSON.stringify(err));
-          }}
           source={{
-            uri,
+            uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
           }}
-          controls
-          style={{
-            width: Dimensions.get('window').width,
-            height: 300,
-            backgroundColor: '#fb0',
-          }}
+          style={{width: 400, height: 300}}
         />
-        <Text>{error}</Text>
+        {isLoading && <Text>loading...</Text>}
+        {user && (
+          <Text style={{fontSize: 30, fontWeight: 'bold'}}>{user.email}</Text>
+        )}
+        {user && <Text>{JSON.stringify(user, null, 2)}</Text>}
+        {!user && (
+          <TextInput
+            value={username}
+            onChangeText={value => setUsername(value)}
+            style={{backgroundColor: 'white', color: 'black'}}
+            placeholder="email"
+          />
+        )}
+        <View style={{height: 10}} />
+        {!user && (
+          <TextInput
+            value={password}
+            onChangeText={value => setPassword(value)}
+            style={{backgroundColor: 'white', color: 'black'}}
+            placeholder="password"
+          />
+        )}
+        {!user && (
+          <Button
+            onPress={login}
+            title="login"
+            disabled={isLoading || isInit}
+          />
+        )}
+        {user && (
+          <Button
+            onPress={logout}
+            title="logout"
+            disabled={isLoading || isInit}
+          />
+        )}
       </ScrollView>
     </SafeAreaView>
   );
 };
 
-export default App;
+export default AuthApp;
